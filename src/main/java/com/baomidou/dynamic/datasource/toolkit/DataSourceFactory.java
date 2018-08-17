c20996ba2c3eabf911ebdd707309c2137434c38c
小锅盖 <332309254@qq.com>
2018-08-18 01:17:26 +0800

    refactor: 重构数据源创建过程

---
+++ src/main/java/com/baomidou/dynamic/datasource/toolkit/DataSourceFactory.java
@@ -0,0 +1,141 @@
+/**
+ * Copyright © 2018 organization 苞米豆
+ * <pre>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <pre/>
+ */
+package com.baomidou.dynamic.datasource.toolkit;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.DataSourceProperty;
+import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.druid.DruidDataSourceProperties;
+
+import javax.sql.DataSource;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.sql.SQLException;
+
+/**
+ * 数据源工厂
+ *
+ * @author TaoYu
+ * @since 2.1.0
+ */
+public class DataSourceFactory {
+
+    /**
+     * Ali DruidD 数据源
+     */
+    public static final String DRUID_DATASOURCE = "com.alibaba.druid.pool.DruidDataSource";
+
+    private static Method createMethod;
+    private static Method typeMethod;
+    private static Method urlMethod;
+    private static Method usernameMethod;
+    private static Method passwordMethod;
+    private static Method driverClassNameMethod;
+    private static Method buildMethod;
+
+    static {
+        Class builderClass = null;
+        try {
+            builderClass = Class.forName("org.springframework.boot.jdbc.DataSourceBuilder");
+        } catch (Exception e) {
+            try {
+                builderClass = Class.forName("org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder");
+            } catch (Exception e1) {
+            }
+        }
+        try {
+            createMethod = builderClass.getDeclaredMethod("create");
+            typeMethod = builderClass.getDeclaredMethod("type", Class.class);
+            urlMethod = builderClass.getDeclaredMethod("url", String.class);
+            usernameMethod = builderClass.getDeclaredMethod("username", String.class);
+            passwordMethod = builderClass.getDeclaredMethod("password", String.class);
+            driverClassNameMethod = builderClass.getDeclaredMethod("driverClassName", String.class);
+            buildMethod = builderClass.getDeclaredMethod("build");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 创建数据源
+     *
+     * @param dataSourceProperty 数据源信息
+     * @return 数据源
+     */
+    public static DataSource createDataSource(DataSourceProperty dataSourceProperty) {
+        Class<? extends DataSource> type = dataSourceProperty.getType();
+        if (type == null) {
+            try {
+                Class.forName(DRUID_DATASOURCE);
+                return createDruidDataSource(dataSourceProperty);
+            } catch (ClassNotFoundException e) {
+            }
+        } else if (DRUID_DATASOURCE.equals(type.getName())) {
+            return createDruidDataSource(dataSourceProperty);
+        }
+        return createBasicDataSource(dataSourceProperty);
+    }
+
+    public static DataSource createBasicDataSource(DataSourceProperty dataSourceProperty) {
+        try {
+            Object o1 = createMethod.invoke(null);
+            Object o2 = typeMethod.invoke(o1, dataSourceProperty.getType());
+            Object o3 = urlMethod.invoke(o2, dataSourceProperty.getUrl());
+            Object o4 = usernameMethod.invoke(o3, dataSourceProperty.getUsername());
+            Object o5 = passwordMethod.invoke(o4, dataSourceProperty.getPassword());
+            Object o6 = driverClassNameMethod.invoke(o5, dataSourceProperty.getDriverClassName());
+            return (DataSource) buildMethod.invoke(o6);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static DataSource createDruidDataSource(DataSourceProperty dataSourceProperty) {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setUrl(dataSourceProperty.getUrl());
+        druidDataSource.setUsername(dataSourceProperty.getUsername());
+        druidDataSource.setPassword(dataSourceProperty.getPassword());
+        druidDataSource.setDriverClassName(dataSourceProperty.getDriverClassName());
+
+        DruidDataSourceProperties druidProperties = dataSourceProperty.getDruid();
+
+        druidDataSource.setInitialSize(druidProperties.getInitialSize());
+        druidDataSource.setMaxActive(druidProperties.getMaxActive());
+        druidDataSource.setMinIdle(druidProperties.getMinIdle());
+        druidDataSource.setMaxWait(druidProperties.getMaxWait());
+        druidDataSource.setTimeBetweenEvictionRunsMillis(druidProperties.getTimeBetweenEvictionRunsMillis());
+        druidDataSource.setMinEvictableIdleTimeMillis(druidProperties.getMinEvictableIdleTimeMillis());
+        druidDataSource.setMaxEvictableIdleTimeMillis(druidProperties.getMaxEvictableIdleTimeMillis());
+        druidDataSource.setValidationQuery(druidProperties.getValidationQuery());
+        druidDataSource.setValidationQueryTimeout(druidProperties.getValidationQueryTimeout());
+        druidDataSource.setTestOnBorrow(druidProperties.isTestOnBorrow());
+        druidDataSource.setTestOnReturn(druidProperties.isTestOnReturn());
+        druidDataSource.setPoolPreparedStatements(druidProperties.isPoolPreparedStatements());
+        druidDataSource.setMaxOpenPreparedStatements(druidProperties.getMaxOpenPreparedStatements());
+        druidDataSource.setSharePreparedStatements(druidProperties.isSharePreparedStatements());
+        druidDataSource.setConnectProperties(druidProperties.getConnectionProperties());
+        try {
+            druidDataSource.setFilters(druidProperties.getFilters());
+            druidDataSource.init();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return druidDataSource;
+    }
+}
