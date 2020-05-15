d5b35f3b5d35ec571af721c3b00287d07c583c73
刘尚 <liushang@zsyjr.com>
2020-05-15 16:37:47 +0800

    加入license, 类的作者

--- src/main/java/com/baomidou/dynamic/datasource/creator/DbcpDataSourceCreator.java
+++ src/main/java/com/baomidou/dynamic/datasource/creator/DbcpDataSourceCreator.java
@@ -1,16 +1,36 @@
+/**
+ * Copyright © 2018 organization baomidou
+ * <pre>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <pre/>
+ */
 package com.baomidou.dynamic.datasource.creator;
 
 import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.DataSourceProperty;
 import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.dbcp.DbcpConfig;
 import org.apache.commons.dbcp.BasicDataSource;
-import org.apache.commons.lang.ObjectUtils;
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.PropertyAccessorFactory;
 
 import javax.sql.DataSource;
 import java.beans.PropertyDescriptor;
-
+/**
+ * Dbcp数据源创建器
+ *
+ * @author liushang
+ * @since 2020/1/21
+ */
 public class DbcpDataSourceCreator {
 
     private DbcpConfig dbcpConfig;
