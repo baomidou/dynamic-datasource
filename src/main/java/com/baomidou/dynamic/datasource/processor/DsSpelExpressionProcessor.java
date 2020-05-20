01b0eb85689c7b0faf7d3eee715eb1960676ad0a
刘尚 <liushang@zsyjr.com>
2020-05-20 11:16:55 +0800

    支持 spel 表达式的默认格式

--- src/main/java/com/baomidou/dynamic/datasource/processor/DsSpelExpressionProcessor.java
+++ src/main/java/com/baomidou/dynamic/datasource/processor/DsSpelExpressionProcessor.java
@@ -23,6 +23,7 @@ import org.springframework.core.DefaultParameterNameDiscoverer;
 import org.springframework.core.ParameterNameDiscoverer;
 import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.ParserContext;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 
 /**
@@ -45,12 +46,24 @@ public class DsSpelExpressionProcessor extends DsProcessor {
     return true;
   }
 
+  /**
+   * 解析上下文的模板
+   * 对于默认不设置的情况下,从参数中取值的方式 #param1
+   * 设置指定模板 ParserContext.TEMPLATE_EXPRESSION 后的取值方式: #{#param1}
+   * issues: https://github.com/baomidou/dynamic-datasource-spring-boot-starter/issues/199
+   */
+  private ParserContext parserContext = null;
+
   @Override
   public String doDetermineDatasource(MethodInvocation invocation, String key) {
     Method method = invocation.getMethod();
     Object[] arguments = invocation.getArguments();
     EvaluationContext context = new MethodBasedEvaluationContext(null, method, arguments, NAME_DISCOVERER);
-    final Object value = PARSER.parseExpression(key).getValue(context);
+    final Object value = PARSER.parseExpression(key,parserContext).getValue(context);
     return value == null ? null : value.toString();
+  }
+
+  public void setParserContext(ParserContext parserContext) {
+    this.parserContext = parserContext;
   }
 }
