61897819415269878087a7611283fdda55e3e2d7
刘尚 <liushang@zsyjr.com>
2020-05-20 11:16:55 +0800

    支持 spel 表达式的默认格式

--- src/main/java/com/baomidou/dynamic/datasource/spring/boot/autoconfigure/DynamicDataSourceAutoConfiguration.java
+++ src/main/java/com/baomidou/dynamic/datasource/spring/boot/autoconfigure/DynamicDataSourceAutoConfiguration.java
@@ -43,6 +43,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.Ordered;
+import org.springframework.expression.ParserContext;
 
 /**
  * 动态数据源核心自动配置类
