4ddfc4df335845c7d41d18779153dbdbb16f84ec
小锅盖 <332309254@qq.com>
2020-05-21 11:35:50 +0800

    feat: 设置默认spel解析方式为空

--- src/main/java/com/baomidou/dynamic/datasource/processor/DsSpelExpressionProcessor.java
+++ src/main/java/com/baomidou/dynamic/datasource/processor/DsSpelExpressionProcessor.java
@@ -47,7 +47,23 @@ public class DsSpelExpressionProcessor extends DsProcessor {
      * 设置指定模板 ParserContext.TEMPLATE_EXPRESSION 后的取值方式: #{#param1}
      * issues: https://github.com/baomidou/dynamic-datasource-spring-boot-starter/issues/199
      */
-    private ParserContext parserContext = null;
+    private ParserContext parserContext = new ParserContext() {
+
+        @Override
+        public boolean isTemplate() {
+            return false;
+        }
+
+        @Override
+        public String getExpressionPrefix() {
+            return null;
+        }
+
+        @Override
+        public String getExpressionSuffix() {
+            return null;
+        }
+    };
 
     @Override
     public boolean matches(String key) {
