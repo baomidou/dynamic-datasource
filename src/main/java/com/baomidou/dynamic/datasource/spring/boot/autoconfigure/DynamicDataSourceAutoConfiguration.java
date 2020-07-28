6415b94949c8c6cc903bbf10519d08c58069a06a
刘尚 <liushang@zsyjr.com>
2020-07-28 22:08:29 +0800

    fixed: 支持 InfrastructureAdvisorAutoProxyCreator , spring.aop.auto=false后,仍然可以正常代理

--- src/main/java/com/baomidou/dynamic/datasource/spring/boot/autoconfigure/DynamicDataSourceAutoConfiguration.java
+++ src/main/java/com/baomidou/dynamic/datasource/spring/boot/autoconfigure/DynamicDataSourceAutoConfiguration.java
@@ -29,7 +29,9 @@ import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.druid.DruidDyna
 import com.baomidou.dynamic.datasource.strategy.DynamicDataSourceStrategy;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -37,6 +39,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Role;
+import org.springframework.core.Ordered;
 
 import javax.sql.DataSource;
 import java.util.Map;
@@ -81,6 +85,7 @@ public class DynamicDataSourceAutoConfiguration {
         return dataSource;
     }
 
+    @Role(value = BeanDefinition.ROLE_INFRASTRUCTURE)
     @Bean
     @ConditionalOnMissingBean
     public DynamicDataSourceAnnotationAdvisor dynamicDatasourceAnnotationAdvisor(DsProcessor dsProcessor) {
@@ -101,4 +106,5 @@ public class DynamicDataSourceAutoConfiguration {
         sessionProcessor.setNextProcessor(spelExpressionProcessor);
         return headerProcessor;
     }
+
 }
