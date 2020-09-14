8d59f92ff17aa37f809160c179ec7825d8ece5fa
小锅盖 <332309254@qq.com>
2020-09-14 17:16:48 +0800

    feat: support custom annotation on method lower than spring5.0

--- src/main/java/com/baomidou/dynamic/datasource/aop/DynamicDataSourceAnnotationAdvisor.java
+++ src/main/java/com/baomidou/dynamic/datasource/aop/DynamicDataSourceAnnotationAdvisor.java
@@ -19,20 +19,29 @@ package com.baomidou.dynamic.datasource.aop;
 import com.baomidou.dynamic.datasource.annotation.DS;
 import lombok.NonNull;
 import org.aopalliance.aop.Advice;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.support.AbstractPointcutAdvisor;
+import org.springframework.aop.support.AopUtils;
 import org.springframework.aop.support.ComposablePointcut;
+import org.springframework.aop.support.StaticMethodMatcher;
 import org.springframework.aop.support.annotation.AnnotationMatchingPointcut;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.util.Assert;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 /**
  * @author TaoYu
  * @since 1.2.0
  */
-public class DynamicDataSourceAnnotationAdvisor extends AbstractPointcutAdvisor implements
-        BeanFactoryAware {
+public class DynamicDataSourceAnnotationAdvisor extends AbstractPointcutAdvisor implements BeanFactoryAware {
 
     private final Advice advice;
 
@@ -62,7 +71,56 @@ public class DynamicDataSourceAnnotationAdvisor extends AbstractPointcutAdvisor 
 
     private Pointcut buildPointcut() {
         Pointcut cpc = new AnnotationMatchingPointcut(DS.class, true);
-        Pointcut mpc = AnnotationMatchingPointcut.forMethodAnnotation(DS.class);
+        Pointcut mpc = new AnnotationMethodPoint(DS.class);
         return new ComposablePointcut(cpc).union(mpc);
+    }
+
+    /**
+     * In order to be compatible with the spring lower than 5.0
+     */
+    private static class AnnotationMethodPoint implements Pointcut {
+
+        private final Class<? extends Annotation> annotationType;
+
+        public AnnotationMethodPoint(Class<? extends Annotation> annotationType) {
+            Assert.notNull(annotationType, "Annotation type must not be null");
+            this.annotationType = annotationType;
+        }
+
+        @Override
+        public ClassFilter getClassFilter() {
+            return ClassFilter.TRUE;
+        }
+
+        @Override
+        public MethodMatcher getMethodMatcher() {
+            return new AnnotationMethodMatcher(annotationType);
+        }
+
+        private static class AnnotationMethodMatcher extends StaticMethodMatcher {
+            private final Class<? extends Annotation> annotationType;
+
+            public AnnotationMethodMatcher(Class<? extends Annotation> annotationType) {
+                this.annotationType = annotationType;
+            }
+
+            @Override
+            public boolean matches(Method method, Class<?> targetClass) {
+                if (matchesMethod(method)) {
+                    return true;
+                }
+                // Proxy classes never have annotations on their redeclared methods.
+                if (Proxy.isProxyClass(targetClass)) {
+                    return false;
+                }
+                // The method may be on an interface, so let's check on the target class as well.
+                Method specificMethod = AopUtils.getMostSpecificMethod(method, targetClass);
+                return (specificMethod != method && matchesMethod(specificMethod));
+            }
+
+            private boolean matchesMethod(Method method) {
+                return AnnotatedElementUtils.hasAnnotation(method, this.annotationType);
+            }
+        }
     }
 }
