efd9f1388c822b6aebc29370ad36e899c613f179
小锅盖 <332309254@qq.com>
2021-03-13 21:07:50 +0800

    style:license format

--- src/main/java/com/baomidou/dynamic/datasource/creator/JndiDataSourceCreator.java
+++ src/main/java/com/baomidou/dynamic/datasource/creator/JndiDataSourceCreator.java
@@ -1,6 +1,6 @@
-/**
+/*
  * Copyright © 2018 organization baomidou
- * <pre>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,7 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * <pre/>
  */
 package com.baomidou.dynamic.datasource.creator;
 
