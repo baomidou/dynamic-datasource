cb052b94af87e5c079d2824261bc500075e371c6
hzh <727172424@qq.com>
2021-11-24 14:58:29 +0800

    主要是增加本地多数据源事务传播机制

---
+++ src/main/java/com/baomidou/dynamic/datasource/tx/TransactionalExecutor.java
@@ -0,0 +1,11 @@
+package com.baomidou.dynamic.datasource.tx;
+
+
+
+public interface TransactionalExecutor {
+
+    Object execute() throws Throwable;
+
+
+    TransactionalInfo getTransactionInfo();
+}
