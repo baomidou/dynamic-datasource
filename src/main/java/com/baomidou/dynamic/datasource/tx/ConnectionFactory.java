e3931c27eb66204c56d264b0cc0e3f7acf0cbf68
hzh <727172424@qq.com>
2021-11-29 11:27:10 +0800

    3.5.0事务传播机制代码合并

--- src/main/java/com/baomidou/dynamic/datasource/tx/ConnectionFactory.java
+++ src/main/java/com/baomidou/dynamic/datasource/tx/ConnectionFactory.java
@@ -16,6 +16,7 @@
 package com.baomidou.dynamic.datasource.tx;
 
 import org.springframework.util.CollectionUtils;
+
 import java.sql.SQLException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
