5dd1255df70020f0604b6d73324a2e9a73bfaf4c
HuaYan <tracy5546@gmail.com>
2023-07-09 21:16:04 +0800

    style: format

--- dynamic-datasource-spring/src/main/java/com/baomidou/dynamic/datasource/tx/LocalTxUtil.java
+++ dynamic-datasource-spring/src/main/java/com/baomidou/dynamic/datasource/tx/LocalTxUtil.java
@@ -17,6 +17,7 @@ package com.baomidou.dynamic.datasource.tx;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StringUtils;
+
 import java.security.SecureRandom;
 import java.util.UUID;
 
@@ -36,24 +37,24 @@ public final class LocalTxUtil {
         }
     };
 
-    public static UUID randomUUID(){
+    public static UUID randomUUID() {
         SecureRandom ng = SECURE_RANDOM_HOLDER.get();
         byte[] randomBytes = new byte[16];
         ng.nextBytes(randomBytes);
         // clear version
-        randomBytes[6]  &= 0x0f;
+        randomBytes[6] &= 0x0f;
         // set to version 4
-        randomBytes[6]  |= 0x40;
+        randomBytes[6] |= 0x40;
         // clear variant
-        randomBytes[8]  &= 0x3f;
+        randomBytes[8] &= 0x3f;
         // set to IETF variant
-        randomBytes[8]  |= 0x80;
+        randomBytes[8] |= 0x80;
         long msb = 0;
         long lsb = 0;
-        for (int i=0; i<8; i++) {
+        for (int i = 0; i < 8; i++) {
             msb = (msb << 8) | (randomBytes[i] & 0xff);
         }
-        for (int i=8; i<16; i++) {
+        for (int i = 8; i < 16; i++) {
             lsb = (lsb << 8) | (randomBytes[i] & 0xff);
         }
         return new UUID(msb, lsb);
