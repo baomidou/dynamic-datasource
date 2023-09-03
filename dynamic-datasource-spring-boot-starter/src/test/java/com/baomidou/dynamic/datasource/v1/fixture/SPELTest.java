4dbf6ebd6f89e8544d9107138c9f419e00745a8a
linghengqian <linghengqian@outlook.com>
2023-09-03 21:42:10 +0800

    Change the packages of different Spring Boot Starter submodules to avoid generating conflicting GraalVM Reachability Metadata

---
+++ dynamic-datasource-spring-boot-starter/src/test/java/com/baomidou/dynamic/datasource/v1/fixture/SPELTest.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright © 2018 organization baomidou
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.baomidou.dynamic.datasource.v1.fixture;
+
+import com.baomidou.dynamic.datasource.DynamicRoutingDataSource;
+import com.baomidou.dynamic.datasource.creator.DataSourceProperty;
+import com.baomidou.dynamic.datasource.creator.DefaultDataSourceCreator;
+import com.baomidou.dynamic.datasource.v1.fixture.service.spel.CustomResultHandler;
+import com.baomidou.dynamic.datasource.v1.fixture.service.spel.User;
+import com.baomidou.dynamic.datasource.v1.fixture.service.spel.UserService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.sql.DataSource;
+import java.nio.charset.StandardCharsets;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@SpringBootTest(classes = SPELApplication.class, webEnvironment = RANDOM_PORT)
+@RunWith(SpringRunner.class)
+public class SPELTest {
+
+    MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    DefaultDataSourceCreator dataSourceCreator;
+
+    @Autowired
+    UserService userService;
+
+    @Before
+    public void setup() {
+        this.mockMvc = webAppContextSetup(webApplicationContext)
+                .alwaysDo(new CustomResultHandler())
+                .build();
+    }
+
+    @Test
+    public void testSPEL() throws Exception {
+        DataSourceProperty masterDataSourceProperty = createDataSourceProperty("master");
+        DataSourceProperty tenant1_1DataSourceProperty = createDataSourceProperty("tenant1_1");
+        DataSourceProperty tenant1_2DataSourceProperty = createDataSourceProperty("tenant1_2");
+        DataSourceProperty tenant2_1DataSourceProperty = createDataSourceProperty("tenant2_1");
+        DataSourceProperty tenant2_2DataSourceProperty = createDataSourceProperty("tenant2_2");
+        DynamicRoutingDataSource ds = (DynamicRoutingDataSource) dataSource;
+        ds.addDataSource(masterDataSourceProperty.getPoolName(), dataSourceCreator.createDataSource(masterDataSourceProperty));
+        ds.addDataSource(tenant1_1DataSourceProperty.getPoolName(), dataSourceCreator.createDataSource(tenant1_1DataSourceProperty));
+        ds.addDataSource(tenant1_2DataSourceProperty.getPoolName(), dataSourceCreator.createDataSource(tenant1_2DataSourceProperty));
+        ds.addDataSource(tenant2_1DataSourceProperty.getPoolName(), dataSourceCreator.createDataSource(tenant2_1DataSourceProperty));
+        ds.addDataSource(tenant2_2DataSourceProperty.getPoolName(), dataSourceCreator.createDataSource(tenant2_2DataSourceProperty));
+        assertThat(ds.getDataSources().keySet()).contains("master", "tenant1_1", "tenant1_2", "tenant2_1", "tenant2_2");
+        mockMvc.perform(MockMvcRequestBuilders.get("/users/session").characterEncoding(StandardCharsets.UTF_8.name()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().encoding(StandardCharsets.UTF_8.name()))
+                .andReturn().getResponse().getContentAsString();
+        mockMvc.perform(MockMvcRequestBuilders.get("/users/header").contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .header("tenantName", "tenant1")
+                        .characterEncoding(StandardCharsets.UTF_8.name())
+                ).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().encoding(StandardCharsets.UTF_8.name()))
+                .andReturn().getResponse().getContentAsString();
+        assertThat(userService.selectSpelByKey("tenant1")).isEqualTo("tenant1");
+        assertThat(userService.selecSpelByTenant(new User("tenant2"))).isEqualTo("tenant2");
+    }
+
+    private DataSourceProperty createDataSourceProperty(String poolName) {
+        DataSourceProperty result = new DataSourceProperty();
+        result.setPoolName(poolName);
+        result.setDriverClassName("org.h2.Driver");
+        result.setUrl("jdbc:h2:mem:" + poolName + ";INIT=RUNSCRIPT FROM 'classpath:db/spring-expression-language.sql'");
+        result.setUsername("sa");
+        result.setPassword("");
+        return result;
+    }
+}
+
+@SpringBootApplication
+class SPELApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SPELApplication.class, args);
+    }
+}
