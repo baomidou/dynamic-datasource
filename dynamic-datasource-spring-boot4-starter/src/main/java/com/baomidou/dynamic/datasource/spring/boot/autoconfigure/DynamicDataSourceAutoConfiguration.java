97b5236a582a654315d0c9e15a7dbf94d0285dd7
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2025-11-28 10:15:45 +0800

    feat: 支持 spring-boot 4.0.0 (#738)

---
+++ dynamic-datasource-spring-boot4-starter/src/main/java/com/baomidou/dynamic/datasource/spring/boot/autoconfigure/DynamicDataSourceAutoConfiguration.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright © 2018 organization baomidou
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.baomidou.dynamic.datasource.spring.boot.autoconfigure;
+
+import com.baomidou.dynamic.datasource.DynamicRoutingDataSource;
+import com.baomidou.dynamic.datasource.provider.DynamicDataSourceProvider;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.util.CollectionUtils;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+/**
+ * 动态数据源核心自动配置类
+ *
+ * @author TaoYu Kanyuxia
+ * @since 1.0.0
+ */
+@Slf4j
+@Configuration(proxyBeanMethods = false)
+@AutoConfigureBefore(
+        value = DataSourceAutoConfiguration.class,
+        name = {
+                "com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure",
+                "com.alibaba.druid.spring.boot4.autoconfigure.DruidDataSourceAutoConfigure"
+        })
+@Import({DruidDynamicDataSourceConfiguration.class, DynamicDataSourceCreatorAutoConfiguration.class, DynamicDataSourceAopConfiguration.class, DynamicDataSourceAssistConfiguration.class})
+@ConditionalOnProperty(prefix = DynamicDataSourceProperties.PREFIX, name = "enabled", havingValue = "true", matchIfMissing = true)
+public class DynamicDataSourceAutoConfiguration implements InitializingBean {
+
+    private final DynamicDataSourceProperties properties;
+
+    private final List<DynamicDataSourcePropertiesCustomizer> dataSourcePropertiesCustomizers;
+
+    public DynamicDataSourceAutoConfiguration(
+            DynamicDataSourceProperties properties,
+            ObjectProvider<List<DynamicDataSourcePropertiesCustomizer>> dataSourcePropertiesCustomizers) {
+        this.properties = properties;
+        this.dataSourcePropertiesCustomizers = dataSourcePropertiesCustomizers.getIfAvailable();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public DataSource dataSource(List<DynamicDataSourceProvider> providers) {
+        DynamicRoutingDataSource dataSource = new DynamicRoutingDataSource(providers);
+        dataSource.setPrimary(properties.getPrimary());
+        dataSource.setStrict(properties.getStrict());
+        dataSource.setStrategy(properties.getStrategy());
+        dataSource.setP6spy(properties.getP6spy());
+        dataSource.setSeata(properties.getSeata());
+        dataSource.setGraceDestroy(properties.getGraceDestroy());
+        return dataSource;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        if (!CollectionUtils.isEmpty(dataSourcePropertiesCustomizers)) {
+            for (DynamicDataSourcePropertiesCustomizer customizer : dataSourcePropertiesCustomizers) {
+                customizer.customize(properties);
+            }
+        }
+    }
+
+}
